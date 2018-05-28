f7fe003009c892e048cd8afe4b3ca6d341e0fa2b
iespino <iespino69@gmail.com>
2018-05-28 16:00:58 -0500

    28-05-2018

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ function utf8_converter($array)
 
 function updateRepository()
 {
-    $fecha=date("d-m-Y H:i:s");
+    $fecha=date("d-m-Y");
     exec("git add .");
     exec('git commit -m "'.$fecha.'" ');
     exec('git push -u origin master');
